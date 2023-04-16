e918f74e945d853c5f9b9180f2e01fd517abf8a5
MARCOS LOURENCO DE SOUZA <30378367+skymarkos7@users.noreply.github.com>
2023-04-16 18:05:56 -0300

    map

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Quasar App (marvel-comics)
 
 listagem de quadrinhos marvel
+
 # Using framework to vue (quasar)
+
 ## Install the dependencies
+
 ```bash
 yarn
 # or
@@ -10,58 +13,59 @@ npm install
 ```
 
 ### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
 ```bash
 quasar dev
 # or
 npx quasar dev
 ```
 
-
 ### Lint the files
+
 ```bash
 yarn lint
 # or
 npm run lint
 ```
 
-
 ### Format the files
+
 ```bash
 yarn format
 # or
 npm run format
 ```
 
-
-
 ### Build the app for production
+
 ```bash
 quasar build
 # or
 npx quasar build
 ```
 
-
-
 # About
+
 ## characteristics:
+
 - Screem of load
 - api with axios
   - autentication api for get infos
     - apikey public
     - ts actualy
     - hash = apikey public + apikey private + ts
+- google maps
+  - select address for clicking, zipcode, country, city etc.
+  - iframe used to enable greater interaction with google maps.
 - versioning
+
   - Using github on branch **develop** for development
 
-
   ## pendencia
+
   - carregar paginação
   - permitir compartilhar
   - aplicar transparência
   - possibilidade de filtro
   - favoritar
   - atualizar o hash a cada chamada
-
-
-
--- src/pages/SendToMap.vue
+++ src/pages/SendToMap.vue
@@ -1,17 +1,59 @@
 <template>
-  <div style="width: 60%; height: 700px" ref="page"></div>
+  <button @click="data()">cammom data</button>
+  <div class="row">
+    <div style="width: 400px !important">
+      <form style="width: 200px" action="">
+        <div class="card-container">
+          <div class="panel">
+            <div>
+              <img
+                class="sb-title-icon"
+                src="https://fonts.gstatic.com/s/i/googlematerialicons/location_pin/v5/24px.svg"
+                alt=""
+              />
+              <span class="sb-title">Address Selection</span>
+            </div>
+            <input type="text" placeholder="Address" id="location-input" />
+            <input type="text" placeholder="Apt, Suite, etc (optional)" />
+            <input type="text" placeholder="City" id="locality-input" />
+            <div class="half-input-container">
+              <input
+                type="text"
+                class="half-input"
+                placeholder="State/Province"
+                id="administrative_area_level_1-input"
+              />
+              <input
+                type="text"
+                class="half-input"
+                placeholder="Zip/Postal code"
+                id="postal_code-input"
+              />
+            </div>
+            <input type="text" placeholder="Country" id="country-input" />
+            <button class="button-cta">Send to</button>
+          </div>
+          <div class="map" id="gmp-map"></div>
+        </div>
+      </form>
+    </div>
+
+    <div style="width: 50%; min-width: 300px; height: 600px" ref="page"></div>
+  </div>
 </template>
 
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      datas: "",
+    };
   },
   mounted: function () {
     const page = this.$refs.page;
     const iframe = document.createElement("iframe");
-    iframe.src = "/pages/index.html";
+    iframe.src = "/pageMap/index.html";
     iframe.style.width = "100%";
     iframe.style.height = "100%";
     iframe.onload = () => {
@@ -21,17 +63,104 @@ export default {
       const style = document.createElement("link");
       style.rel = "stylesheet";
       style.type = "text/css";
-      style.href = "/pages/style.css";
+      style.href = "/pageMap/style.css";
       head.appendChild(style);
       const script = document.createElement("script");
       script.type = "text/javascript";
-      script.src = "/pages/script.js";
+      script.src = "/pageMap/script.js";
       body.appendChild(script);
     };
     page.appendChild(iframe);
+
+    this.datas = iframe;
   },
-  methods: {},
+  methods: {
+    data() {
+      console.log(this.datas);
+    },
+  },
 };
 </script>
+
+
+
+
+<style scoped>
+.sb-title {
+  position: relative;
+  top: -12px;
+  font-family: Roboto, sans-serif;
+  font-weight: 500;
+}
+
+.sb-title-icon {
+  position: relative;
+  top: -5px;
+}
+
+.card-container {
+  display: flex;
+  height: 30%;
+  width: 600px;
+}
+
+.panel {
+  background: white;
+  width: 300px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.half-input-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.half-input {
+  max-width: 120px;
+}
+
+.map {
+  width: 300px;
+}
+
+h2 {
+  margin: 0;
+  font-family: Roboto, sans-serif;
+}
+
+input {
+  height: 30px;
+}
+
+input {
+  border: 0;
+  border-bottom: 1px solid black;
+  font-size: 14px;
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+}
+
+input:focus::placeholder {
+  color: white;
+}
+
+.button-cta {
+  height: 40px;
+  width: 40%;
+  background: #3367d6;
+  color: white;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: Roboto, sans-serif;
+  border: 0;
+  border-radius: 3px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.48);
+  cursor: pointer;
+}
+</style>
 
 
